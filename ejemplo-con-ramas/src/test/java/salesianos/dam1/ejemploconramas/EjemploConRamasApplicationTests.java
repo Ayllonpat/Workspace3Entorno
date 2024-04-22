0f3d1a4c3c0345a2e8fd273a0324942d0a814256
Patricia Alonso Ayllón <alonso.aypat22@triana.salesianos.edu>
2024-04-22 13:27:24 +0200

    ramas

---
+++ ejemplo-con-ramas/src/test/java/salesianos/dam1/ejemploconramas/EjemploConRamasApplicationTests.java
@@ -0,0 +1,13 @@
+package salesianos.dam1.ejemploconramas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemploConRamasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
